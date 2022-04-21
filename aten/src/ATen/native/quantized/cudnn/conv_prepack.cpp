7e41df79ba4623cbacb0525180c076b19fc08b82
dzdang <dzdang@umich.edu>
2022-04-21 14:57:16 -0700

    [quant][core][gpu][improvement] Added support for padding quantized cudnn conv2d operator
    Summary:
    cudnn v8.4.0 expects input channels for conv2d to be a multiple of 4. If
    it is not, we need to explicitly pad it to a multiple of 4 ourselves as
    cudnn does not currently support padding intriniscally.
    The padding implemented here is limited to groups=1; however, this
    should be a straightforward adaption to groups > 1 since we're only
    padding a single dimension.
    
    When cudnn enables support for padding, we can remove the padding on our
    end.
    
    Test plan:
    ```
    python test/test_quantization.py -k test_qconv2d_cudnn
    ```
    
    ghstack-source-id: 6ae71ff41bc645fb832a8c0956e47919fab18338
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/76184

--- aten/src/ATen/native/quantized/cudnn/conv_prepack.cpp
+++ aten/src/ATen/native/quantized/cudnn/conv_prepack.cpp
@@ -72,6 +72,22 @@ c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>> PackedConvWeightCudnn<
     // bias_contig = bias->contiguous();
   }
 
+  // cudnn v8.4.0 expects conv2d's weight tensor's input and output channels to be a multiple of 4. if it is not
+  // we need to explicitly pad it to a multiple of 4 ourselves as cudnn does not currently support padding.
+  // TODO: when and if cudnn enables padding in their operators, we can remove padding on our end;
+  // currently, limit padding support to groups=1 (ungrouped conv)
+  // TODO: implement this for groups > 1
+  auto num_output_channels = weight.size(0);
+  if (weight.size(0) % 4 != 0 || weight.size(1) % 4 != 0) {
+    TORCH_CHECK(groups == 1, "Quantized cudnn conv2d is currenty lmited to groups = 1; received groups =", groups);
+    int8_t nslices2pad_out = (4 - weight.size(0) % 4) % 4; // number of slices we need to pad/add for output channels
+    int8_t nslices2pad_in = (4 - weight.size(1) % 4) % 4; // number of slices we need to pad/add for input channels
+    weight = at::pad(weight, {0, 0, 0, 0, 0, nslices2pad_in, 0, nslices2pad_out}, "constant", 0);
+    if (bias.has_value()) {
+      bias.value() = at::pad(bias.value(), {0, nslices2pad_out}, "constant", 0);
+    }
+  }
+
   auto ret_ptr = c10::make_intrusive<PackedConvWeightCudnn<kSpatialDim>>(
           weight.to(c10::MemoryFormat::ChannelsLast), // TODO: this assumes 2D I think. make it more general?
           bias,
@@ -81,7 +97,8 @@ c10::intrusive_ptr<ConvPackedParamsBase<kSpatialDim>> PackedConvWeightCudnn<
           dilation,
           groups,
           transpose,
-          qtype);
+          qtype,
+          num_output_channels);
   return ret_ptr;
 }
 
